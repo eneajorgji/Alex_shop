0a27ae4a74a39fcedcc047b857f65770ec2e3bc1
Enea Jorgji <jorgji.enea@gmail.cm>
2021-04-11 01:30:19 +0200

    edited file App.vue where was added burger menu and was cleaned file Home.vue

--- alex_shop_vue/src/App.vue
+++ alex_shop_vue/src/App.vue
@@ -4,22 +4,22 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Alex Shop</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="nabar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="nabar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
-          <router-link to="/summer" class="navabr-item">Summer</router-link>
-          <router-link to="/winter" class="navabr-item">Winter</router-link>
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
 
           <div class="navbar-item">
             <div class="buttons">
-              <router-link to="/log-in" class="button-is-light">Log in</router-link>
-              <router-link to="/cart" class="button-is-success">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+              <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping"></i></span>
                 <span>Cart</span>
               </router-link>
@@ -39,6 +39,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
