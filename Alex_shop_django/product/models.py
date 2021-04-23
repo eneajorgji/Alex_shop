9a3859fc7cb903a8cf045b6ba6a6f6c3cec5d5b0
Enea Jorgji <jorgji.enea@gmail.cm>
2021-04-23 22:44:10 +0200

    add to django

--- Alex_shop_django/product/models.py
+++ Alex_shop_django/product/models.py
@@ -1,3 +1,7 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 
 class Category(models.Model):
@@ -11,4 +15,23 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return f'/{self.slug}/' 
+        return f'/{self.slug}/'
+
+class Product(models.Module):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
